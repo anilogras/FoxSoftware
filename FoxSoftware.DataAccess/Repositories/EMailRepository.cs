3fcb5e952f7f725512c5d7aef2a79a2c5670026b
Anıl OĞRAŞ <aogras@asyasoft.com.tr>
2022-08-20 11:43:51 +0300

    Email rep. Create

---
+++ FoxSoftware.DataAccess/Repositories/EMailRepository.cs
@@ -0,0 +1,17 @@
+﻿using FoxSoftware.Entites.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxSoftware.DataAccess.Repositories
+{
+    public class EMailRepository : GenericRepository<Email>
+    {
+        public EMailRepository(Context context) : base(context)
+        {
+
+        }
+    }
+}
